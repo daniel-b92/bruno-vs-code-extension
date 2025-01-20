bdedef970ee81bf9cad54b09e19fdb4f49955581
daniel-b92 <danielbeicht@web.de>
2025-01-20 18:59:15 +0100

    fix update for ancestor test items on deletion

--- src/vsCodeTestTree/handleTestItemDeletion.ts
+++ src/vsCodeTestTree/handleTestItemDeletion.ts
@@ -2,8 +2,34 @@ import { EventEmitter, TestController, Uri } from "vscode";
 import { TestCollection } from "../model/testCollection";
 import { getTestId } from "../testTreeHelper";
 import { getParentItem } from "./parentItemHelper";
+import { dirname } from "path";
+import { existsSync } from "fs";
 
 export const handleTestItemDeletion = (
+    controller: TestController,
+    collection: TestCollection,
+    fileChangedEmitter: EventEmitter<Uri>,
+    uri: Uri
+) => {
+    deleteSingleTestItem(controller, collection, fileChangedEmitter, uri);
+
+    let currentPath = uri.fsPath;
+
+    while (
+        !existsSync(dirname(currentPath)) &&
+        currentPath != collection.rootDirectory
+    ) {
+        deleteSingleTestItem(
+            controller,
+            collection,
+            fileChangedEmitter,
+            Uri.file(currentPath)
+        );
+        currentPath = dirname(currentPath);
+    }
+};
+
+const deleteSingleTestItem = (
     controller: TestController,
     collection: TestCollection,
     fileChangedEmitter: EventEmitter<Uri>,
@@ -12,15 +38,16 @@ export const handleTestItemDeletion = (
     controller.items.delete(getTestId(uri));
     fileChangedEmitter.fire(uri);
 
-    const parentItem = getParentItem(uri, collection);
-    if (parentItem) {
-        parentItem.children.delete(getTestId(uri));
-        fileChangedEmitter.fire(parentItem.uri!);
-    }
     const keyToDelete = Array.from(collection.testData.keys()).find(
-        (item) => item.uri == uri
+        (item) => item.uri?.fsPath == uri.fsPath
     );
     if (keyToDelete) {
         collection.testData.delete(keyToDelete);
+    }
+
+    const parentItem = getParentItem(Uri.file(uri.fsPath), collection);
+    if (parentItem) {
+        parentItem.children.delete(getTestId(uri));
+        fileChangedEmitter.fire(parentItem.uri!);
     }
 };
