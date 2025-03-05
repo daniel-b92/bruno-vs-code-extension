09f2edadf542b7feccbc9b119a91fbc740abd4f2
daniel-b92 <danielbeicht@web.de>
2025-03-05 19:17:26 +0100

    implement waiting time before flushing deletion queue

--- src/treeView/treeItems/itemDeletionHandler.ts
+++ src/treeView/treeItems/itemDeletionHandler.ts
@@ -9,7 +9,21 @@ export class ItemDeletionHandler {
         private treeItemEventEmitter: vscode.EventEmitter<
             BrunoTreeItem | undefined
         >
-    ) {}
+    ) {
+        this.itemAddedToQueue = new vscode.EventEmitter<BrunoTreeItem>();
+        this.itemAddedToQueue.event((item) => {
+            const itemsBeforeCountdown = this.queuedDeletions.push(item);
+
+            setTimeout(() => {
+                if (this.queuedDeletions.length == itemsBeforeCountdown) {
+                    this.flushDeletionQueue();
+                }
+            }, 500);
+        });
+    }
+
+    private queuedDeletions: BrunoTreeItem[] = [];
+    private itemAddedToQueue: vscode.EventEmitter<BrunoTreeItem>;
 
     public handleItemDeletion(itemPath: string) {
         const maybeRegisteredItem = this.itemRegistry.getItem(itemPath);
@@ -18,18 +32,44 @@ export class ItemDeletionHandler {
             return;
         }
 
-        this.itemRegistry.unregisterItem(itemPath);
-        this.itemRegistry.unregisterAllDescendants(itemPath);
+        this.itemAddedToQueue.fire(maybeRegisteredItem);
+    }
 
-        const maybeParent = this.itemRegistry.getItem(
-            dirname(maybeRegisteredItem.getPath())
-        );
+    private flushDeletionQueue() {
+        const deletedPaths: string[] = [];
 
-        if (maybeParent) {
-            this.treeItemEventEmitter.fire(maybeParent);
-        } else {
-            // If no parent item was found, trigger update for all items (e.g. if item is collection root directory).
-            this.treeItemEventEmitter.fire(undefined);
+        const paths = this.queuedDeletions
+            .map((item) => item.getPath())
+            .sort((a, b) => a.length - b.length);
+
+        while (paths.length > 0) {
+            const toDelete = paths.splice(0, 1)[0];
+
+            this.queuedDeletions.splice(
+                this.queuedDeletions.findIndex(
+                    (item) => item.getPath() == toDelete
+                ),
+                1
+            );
+
+            if (!deletedPaths.some((deleted) => toDelete.includes(deleted))) {
+                // Case where no ancestor item has already been handled.
+                // In this case, we need to unregister the item and all registered descendants.
+                deletedPaths.push(toDelete);
+                this.itemRegistry.unregisterItem(toDelete);
+                this.itemRegistry.unregisterAllDescendants(toDelete);
+
+                const maybeParent = this.itemRegistry.getItem(
+                    dirname(toDelete)
+                );
+
+                if (maybeParent) {
+                    this.treeItemEventEmitter.fire(maybeParent);
+                } else {
+                    // If no parent item was found, trigger update for all items (e.g. if item is collection root directory).
+                    this.treeItemEventEmitter.fire(undefined);
+                }
+            }
         }
     }
 }
