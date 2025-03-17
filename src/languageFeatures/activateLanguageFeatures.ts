c987050bb0468e33528e4f98f6e9c070c04b2d53
daniel-b92 <danielbeicht@web.de>
2025-03-17 18:30:43 +0100

    add comüpletion items for type in meta section

--- src/languageFeatures/activateLanguageFeatures.ts
+++ src/languageFeatures/activateLanguageFeatures.ts
@@ -31,4 +31,30 @@ export function activateLanguageFeatures() {
         },
         sequenceTriggerChar
     );
+
+    languages.registerCompletionItemProvider(
+        { scheme: "file", pattern: "**/*.bru" },
+        {
+            provideCompletionItems(document, position) {
+                const currentText = document.lineAt(position.line).text;
+                const typePattern = /^\s*type:\s*$/;
+
+                if (currentText.match(typePattern)) {
+                    return {
+                        items: [
+                            new CompletionItem(
+                                `${currentText.endsWith(" ") ? "" : " "}http`
+                            ),
+                            new CompletionItem(
+                                `${currentText.endsWith(" ") ? "" : " "}graphql`
+                            ),
+                        ],
+                    };
+                } else {
+                    return undefined;
+                }
+            },
+        },
+        sequenceTriggerChar
+    );
 }
