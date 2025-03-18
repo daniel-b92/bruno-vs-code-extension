1bc2e8b2aee2e3d393435cde2fed831155cfd124
daniel-b92 <danielbeicht@web.de>
2025-03-18 11:13:36 +0100

    display more completion items for bruno fields

---
+++ src/languageFeatures/brunoLanguage/provideBrunoLangCompletionItems.ts
@@ -0,0 +1,77 @@
+import { CompletionItem, DocumentSelector, languages } from "vscode";
+import { getMaxSequenceForRequests } from "../../shared/fileSystem/testFileParsing/getMaxSequenceForRequests";
+import { dirname } from "path";
+
+export function provideBrunoLangCompletionItems() {
+    // meta section
+    registerFixedCompletionItems(/^\s*type:\s*$/, "http", "graphql");
+
+    languages.registerCompletionItemProvider(
+        getDocumentSelector(),
+        {
+            provideCompletionItems(document, position) {
+                const currentText = document.lineAt(position.line).text;
+                const sequencePattern = /^\s*seq:\s*$/;
+
+                if (currentText.match(sequencePattern)) {
+                    return {
+                        items: [
+                            new CompletionItem(
+                                `${currentText.endsWith(" ") ? "" : " "}${
+                                    getMaxSequenceForRequests(
+                                        dirname(document.uri.fsPath)
+                                    ) + 1
+                                }`
+                            ),
+                        ],
+                    };
+                } else {
+                    return undefined;
+                }
+            },
+        },
+        getTriggerChar()
+    );
+
+    // HTTP method section
+    registerFixedCompletionItems(/^\s*body:\s*$/, "none", "json", "xml");
+    registerFixedCompletionItems(/^\s*auth:\s*$/, "none", "basic", "bearer");
+}
+
+function registerFixedCompletionItems(
+    linePattern: RegExp,
+    ...choices: string[]
+) {
+    languages.registerCompletionItemProvider(
+        getDocumentSelector(),
+        {
+            provideCompletionItems(document, position) {
+                const currentText = document.lineAt(position.line).text;
+
+                if (currentText.match(linePattern)) {
+                    return {
+                        items: choices.map(
+                            (choice) =>
+                                new CompletionItem(
+                                    `${
+                                        currentText.endsWith(" ") ? "" : " "
+                                    }${choice}`
+                                )
+                        ),
+                    };
+                } else {
+                    return undefined;
+                }
+            },
+        },
+        getTriggerChar()
+    );
+}
+
+function getDocumentSelector(): DocumentSelector {
+    return { scheme: "file", pattern: "**/*.bru" };
+}
+
+function getTriggerChar() {
+    return ":";
+}
