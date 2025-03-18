1bf42b9f48407283bade7c44aa4648c16a89ac21
daniel-b92 <danielbeicht@web.de>
2025-03-18 19:18:29 +0100

    add all known sections to enum to be able to validate existence e.g.

--- src/shared/requestFileSectionsEnum.ts
+++ src/shared/requestFileSectionsEnum.ts
@@ -1,3 +1,29 @@
 export enum RequestFileSection {
+    // Meta
     Meta = "meta",
+
+    // HTTP methods
+    Get = "get",
+    Post = "post",
+    Put = "put",
+    Delete = "delete",
+    Options = "options",
+    Patch = "patch",
+    Head = "head",
+
+    // Auth
+    BasicAuth = "auth:basic",
+    BearerAuth = "auth:bearer",
+
+    // Body
+    JsonBody = "body:json",
+    XmlBody = "body:xml",
+
+    // Scripts
+    PreRequestScript = "script:pre-request",
+    PostResponseScript = "script:post-response",
+
+    // Other
+    Tests = "tests",
+    Docs = "docs",
 }
