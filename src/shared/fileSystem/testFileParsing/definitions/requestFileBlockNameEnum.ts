4aa0d4c3fa29ef6664e2881f99f2345cbcfd7458
daniel-b92 <danielbeicht@web.de>
2025-04-18 18:26:54 +0200

    add missing block names to enum

--- src/shared/fileSystem/testFileParsing/definitions/requestFileBlockNameEnum.ts
+++ src/shared/fileSystem/testFileParsing/definitions/requestFileBlockNameEnum.ts
@@ -11,19 +11,45 @@ export enum RequestFileBlockName {
     Patch = "patch",
     Head = "head",
 
+    // params
+    QueryParams = "params:query",
+    PathParams = "params:path",
+
+    // headers
+    Headers = "headers",
+
     // Auth
     BasicAuth = "auth:basic",
     BearerAuth = "auth:bearer",
+    AwsSigV4Auth = "auth:awsv4",
+    DigestAuth = "auth:digest",
+    NtlmAuth = "auth:ntlm",
+    OAuth2Auth = "auth:oauth2",
+    WsseAuth = "auth:wsse",
+    ApiKeyAuth = "auth:apikey",
+    
 
     // Body
     JsonBody = "body:json",
     XmlBody = "body:xml",
+    TextBody = "body:text",
+    MultipartFormBody = "body:multipart-form",
+    FormUrlEncodedBody = "body:form-urlencoded",
+    SparqlBody = "body:sparql",
+    FileOrBinaryBody = "body:file",
+
+    // Vars
+    PreRequestVars = "vars:pre-request",
+    PostResponseVars = "vars:post-response",
+
+    // Assertions and tests
+    Assertions = "assert",
+    Tests = "tests",
 
     // Scripts
     PreRequestScript = "script:pre-request",
     PostResponseScript = "script:post-response",
 
     // Other
-    Tests = "tests",
     Docs = "docs",
 }
