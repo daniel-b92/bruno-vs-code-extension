02b41b8e17c5b3325cd7a06b422517e5169daeb8
daniel-b92 <danielbeicht@web.de>
2025-06-08 19:01:23 +0200

    adjust name of temporary js file

--- src/languageFeatures/internal/shared/codeBlocksUtils/getTemporaryJsFileName.ts
+++ src/languageFeatures/internal/shared/codeBlocksUtils/getTemporaryJsFileName.ts
@@ -1,5 +1,5 @@
 import { resolve } from "path";
 
 export function getTemporaryJsFileName(collectionRootDirectory: string) {
-    return resolve(collectionRootDirectory, "__temp.js");
+    return resolve(collectionRootDirectory, "__temp_bru_reference.js");
 }
