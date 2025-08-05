3cdb07fe7b0dfdf06706f3a8ab1e5d9a1f85bffe
daniel-b92 <danielbeicht@web.de>
2025-08-05 09:56:36 +0200

    fix handling of cancellation token for queued requests

--- src/languageFeatures/internal/shared/temporaryJsFilesUpdates/internal/interfaces.ts
+++ src/languageFeatures/internal/shared/temporaryJsFilesUpdates/internal/interfaces.ts
@@ -1,4 +1,4 @@
-import { CancellationToken } from "typescript";
+import { CancellationToken } from "vscode";
 
 export enum TempJsUpdateType {
     Creation = "Creation",
--- src/languageFeatures/internal/shared/temporaryJsFilesUpdates/tempJsFileUpdateQueue.ts
+++ src/languageFeatures/internal/shared/temporaryJsFilesUpdates/tempJsFileUpdateQueue.ts
@@ -3,7 +3,7 @@ import { TemporaryJsFilesRegistry } from "./internal/temporaryJsFilesRegistry";
 import { OutputChannelLogger } from "../../../../shared";
 import { createTemporaryJsFile } from "./internal/createTemporaryJsFile";
 import { deleteTemporaryJsFileForCollection } from "./internal/deleteTemporaryJsFile";
-import { EventEmitter } from "vscode";
+import { CancellationToken, EventEmitter } from "vscode";
 
 export class TempJsFileUpdateQueue {
     constructor(
@@ -28,11 +28,21 @@ export class TempJsFileUpdateQueue {
             id,
         });
 
+        const { cancellationToken: token } = updateRequest;
+
+        if (token.isCancellationRequested) {
+            return;
+        }
+
         return this.queue.length > 1 || this.activeUpdate
-            ? await this.waitForRequestToBeAbleToRun(id).then(() =>
-                  this.triggerUpdate(id),
+            ? await this.waitForRequestToBeAbleToRunOrCancelled(id, token).then(
+                  (shouldRun) => {
+                      if (shouldRun) {
+                          this.triggerUpdate(id, token);
+                      }
+                  },
               )
-            : await this.triggerUpdate(id);
+            : await this.triggerUpdate(id, token);
     }
 
     public dispose() {
@@ -42,33 +52,41 @@ export class TempJsFileUpdateQueue {
         this.activeUpdate = undefined;
     }
 
-    private async waitForRequestToBeAbleToRun(requestId: string) {
-        await new Promise<void>((resolve) => {
+    private async waitForRequestToBeAbleToRunOrCancelled(
+        requestId: string,
+        token: CancellationToken,
+    ) {
+        return await new Promise<boolean>((resolve) => {
+            token.onCancellationRequested(() => {
+                if (this.removeFromQueue(requestId)) {
+                    resolve(false);
+                }
+            });
+
             this.updateCanRunNotifier.event((id) => {
+                if (token.isCancellationRequested) {
+                    resolve(false);
+                }
+
                 if (id == requestId) {
-                    resolve();
+                    resolve(true);
                 }
             });
         });
     }
 
-    private async triggerUpdate(requestId: string) {
-        const matchingRequests = this.queue.filter(
-            (queued) => requestId == queued.id,
-        );
+    private async triggerUpdate(requestId: string, token: CancellationToken) {
+        const { request } = this.getRequestFromQueue(requestId);
 
-        if (matchingRequests.length != 1) {
-            throw new Error(
-                `Could not find exactly one temp JS update request in queue to trigger for ID '${requestId}'.
-                Found ${matchingRequests.length} queued items matching the given ID.`,
-            );
+        if (token.isCancellationRequested) {
+            return;
         }
 
-        this.activeUpdate = matchingRequests[0];
+        this.activeUpdate = request;
 
         const {
             request: { collectionRootFolder, update },
-        } = matchingRequests[0];
+        } = request;
 
         if (update.type == TempJsUpdateType.Creation) {
             await createTemporaryJsFile(
@@ -95,6 +113,42 @@ export class TempJsFileUpdateQueue {
         if (this.queue.length > 0) {
             this.updateCanRunNotifier.fire(this.queue[0].id);
         }
+
+        if (token.isCancellationRequested) {
+            this.logger?.debug(
+                `Cancellation requested for temp JS update with ID '${requestId}' after triggering workspace edit. Could not be aborted.`,
+            );
+        }
+    }
+
+    private removeFromQueue(requestId: string) {
+        if (this.activeUpdate && this.activeUpdate.id == requestId) {
+            this.logger?.warn(
+                `Could not remove temp JS update for id '${requestId}' from queue because it's currently active.`,
+            );
+
+            return false;
+        }
+
+        const { index } = this.getRequestFromQueue(requestId);
+        this.queue.splice(index, 1);
+
+        return true;
+    }
+
+    private getRequestFromQueue(requestId: string) {
+        const matchingRequests = this.queue
+            .map((request, index) => ({ request, index }))
+            .filter(({ request: queued }) => requestId == queued.id);
+
+        if (matchingRequests.length != 1) {
+            throw new Error(
+                `Could not find exactly one temp JS update request in queue for ID '${requestId}'.
+                Found ${matchingRequests.length} queued items matching the given ID.`,
+            );
+        }
+
+        return matchingRequests[0];
     }
 
     private getIdForRequest(request: TempJsUpdateRequest) {
