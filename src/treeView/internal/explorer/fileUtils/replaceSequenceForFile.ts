7a8525bd68f050651d3994c1a92d1c3bed811e26
daniel-b92 <danielbeicht@web.de>
2025-11-09 15:00:16 +0100

    Fix update of sequences when moving file and improve error behavior

--- src/treeView/internal/explorer/fileUtils/replaceSequenceForFile.ts
+++ src/treeView/internal/explorer/fileUtils/replaceSequenceForFile.ts
@@ -4,22 +4,24 @@ import { promisify } from "util";
 
 export async function replaceSequenceForFile(
     filePath: string,
-    newSequence: number
+    newSequence: number,
 ) {
+    if (Number.isNaN(newSequence)) {
+        throw new Error(`New sequence to set for file '${filePath}' is NaN `);
+    }
+
     const originalSequence = await parseSequenceFromMetaBlock(filePath);
 
     if (originalSequence != undefined) {
         await promisify(writeFile)(
             filePath,
-            (
-                await promisify(readFile)(filePath, "utf-8")
-            ).replace(
+            (await promisify(readFile)(filePath, "utf-8")).replace(
                 new RegExp(
                     `${MetaBlockKey.Sequence}:\\s*${originalSequence}\\s*$`,
-                    "m"
+                    "m",
                 ),
-                `${MetaBlockKey.Sequence}: ${newSequence}`
-            )
+                `${MetaBlockKey.Sequence}: ${newSequence}`,
+            ),
         );
     }
 }
