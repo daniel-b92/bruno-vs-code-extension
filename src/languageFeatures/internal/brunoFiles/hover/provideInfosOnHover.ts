ede49351d620dfaf631e59c77cb8c02f25e85ca6
daniel-b92 <danielbeicht@web.de>
2025-11-29 16:02:52 +0100

    refactor code a bit and improve displayed hover

--- src/languageFeatures/internal/brunoFiles/hover/provideInfosOnHover.ts
+++ src/languageFeatures/internal/brunoFiles/hover/provideInfosOnHover.ts
@@ -9,10 +9,12 @@ import {
 } from "vscode";
 import {
     Block,
+    BrunoEnvironmentFile,
+    BrunoFileType,
     Collection,
     CollectionItemProvider,
     getConfiguredTestEnvironment,
-    isBodyBlock,
+    getExtensionForBrunoFiles,
     mapFromVsCodePosition,
     mapToVsCodeRange,
     OutputChannelLogger,
@@ -26,6 +28,7 @@ import { getPositionWithinTempJsFile } from "../shared/codeBlocksUtils/getPositi
 import { mapToRangeWithinBruFile } from "../shared/codeBlocksUtils/mapToRangeWithinBruFile";
 import { waitForTempJsFileToBeInSyncWithBruFile } from "../shared/codeBlocksUtils/waitForTempJsFileToBeInSyncWithBruFile";
 import { TempJsFileUpdateQueue } from "../../shared/temporaryJsFilesUpdates/external/tempJsFileUpdateQueue";
+import { basename } from "path";
 
 interface ProviderParams {
     collection: Collection;
@@ -78,69 +81,54 @@ export function provideInfosOnHover(
                 });
             }
 
-            if (isBodyBlock(blockContainingPosition.name)) {
-                return getHoverForNonCodeBlocks({
-                    collection,
-                    file: { document },
-                    hoverRequest: { position, blockContainingPosition, token },
-                    logger,
-                });
-            }
+            return getHoverForNonCodeBlocks({
+                collection,
+                file: { document },
+                hoverRequest: { position, blockContainingPosition, token },
+                logger,
+            });
         },
     });
 }
 
 function getHoverForNonCodeBlocks({
     collection,
-    file: { document },
-    hoverRequest: { position, token: _token },
-    logger: _logger,
+    file,
+    hoverRequest,
+    logger,
 }: ProviderParams) {
-    const pattern = /{{\S+}}/;
-    let remainingText = document.lineAt(position.line).text;
-    let alreadyCheckedText = "";
-    let variableName: undefined | string = undefined;
-
-    do {
-        const matches = pattern.exec(remainingText);
-
-        if (!matches || matches.length == 0) {
-            break;
-        }
-
-        const containsPosition =
-            position.character >= alreadyCheckedText.length + matches.index &&
-            position.character <=
-                alreadyCheckedText.length + matches.index + matches[0].length;
-
-        if (containsPosition) {
-            variableName = matches[0].substring(
-                matches[0].indexOf("{{") + 2,
-                matches[0].indexOf("}}"),
-            );
-            break;
-        }
-        const currentSectionEnd = matches.index + matches[0].length;
-        alreadyCheckedText = alreadyCheckedText.concat(
-            remainingText.substring(0, currentSectionEnd),
-        );
-
-        remainingText = remainingText.substring(currentSectionEnd);
-    } while (remainingText.length > 0);
-
-    const environmentName = getConfiguredTestEnvironment();
-    const environmentFile = environmentName
-        ? collection.getEnvironmentFile(environmentName)
-        : undefined;
-    const variableDefinition = environmentFile
-        ?.getVariables()
-        .find(({ key }) => variableName == key);
-
-    if (!variableName || !variableDefinition) {
+    const {
+        blockContainingPosition: { name: blockName },
+        token,
+    } = hoverRequest;
+    if (
+        (
+            [
+                RequestFileBlockName.Docs,
+                RequestFileBlockName.Meta,
+                RequestFileBlockName.Settings,
+            ] as string[]
+        ).includes(blockName)
+    ) {
+        // In some blocks, variables do not make sense.
         return undefined;
     }
 
-    return new Hover(new MarkdownString(variableDefinition.value));
+    const variableName = getVariableNameContainingPositionForNonCodeBlock({
+        collection,
+        file,
+        hoverRequest,
+        logger,
+    });
+
+    if (token.isCancellationRequested) {
+        logger?.debug(`Cancellation requested for hover provider.`);
+        return undefined;
+    }
+
+    return variableName
+        ? getHoverForVariable(collection, variableName, token, logger)
+        : undefined;
 }
 
 async function getHoverForCodeBlocks(
@@ -204,4 +192,123 @@ async function getHoverForCodeBlocks(
                 ),
             )
           : resultFromJsFile[0];
+}
+
+function getHoverForVariable(
+    collection: Collection,
+    variableName: string,
+    token: CancellationToken,
+    logger?: OutputChannelLogger,
+) {
+    const tableHeader = `| value | environment name |
+| --------------- | ---------------- |\n`;
+
+    const configuredEnvironmentName = getConfiguredTestEnvironment();
+
+    const matchingEnvironmentFiles = collection
+        .getAllStoredDataForCollection()
+        .filter(
+            ({ item }) =>
+                item.getItemType() == BrunoFileType.EnvironmentFile &&
+                (configuredEnvironmentName
+                    ? basename(item.getPath(), getExtensionForBrunoFiles()) ==
+                      configuredEnvironmentName
+                    : true),
+        )
+        .map(({ item }) => item as BrunoEnvironmentFile);
+
+    if (matchingEnvironmentFiles.length == 0) {
+        return undefined;
+    }
+
+    const matchingVariableDefinitions = matchingEnvironmentFiles
+        .map((item) => {
+            const matchingVariables = item
+                .getVariables()
+                .filter(({ key }) => variableName == key);
+
+            return matchingVariables.length > 0
+                ? {
+                      file: item.getPath(),
+                      matchingVariables,
+                  }
+                : undefined;
+        })
+        .filter((result) => result != undefined);
+
+    if (matchingVariableDefinitions.length == 0) {
+        return undefined;
+    }
+
+    if (token.isCancellationRequested) {
+        logger?.debug(`Cancellation requested for hover provider.`);
+        return undefined;
+    }
+
+    return new Hover(
+        new MarkdownString(
+            tableHeader.concat(
+                matchingVariableDefinitions
+                    .map(({ file, matchingVariables }) => {
+                        const environmentName = basename(
+                            file,
+                            getExtensionForBrunoFiles(),
+                        );
+
+                        return matchingVariables
+                            .map(
+                                ({ value }) =>
+                                    `| ${value} | ${environmentName}  |`,
+                            )
+                            .join("\n");
+                    })
+                    .join("\n"),
+            ),
+        ),
+    );
+}
+
+function getVariableNameContainingPositionForNonCodeBlock({
+    file: { document },
+    hoverRequest: { position, token },
+    logger,
+}: ProviderParams) {
+    const pattern = /{{\S+}}/;
+    let remainingText = document.lineAt(position.line).text;
+    let alreadyCheckedText = "";
+    let variableName: undefined | string = undefined;
+
+    do {
+        if (token.isCancellationRequested) {
+            logger?.debug(`Cancellation requested for hover provider.`);
+            return undefined;
+        }
+
+        const matches = pattern.exec(remainingText);
+
+        if (!matches || matches.length == 0) {
+            return undefined;
+        }
+
+        const containsPosition =
+            position.character >= alreadyCheckedText.length + matches.index &&
+            position.character <=
+                alreadyCheckedText.length + matches.index + matches[0].length;
+
+        if (containsPosition) {
+            variableName = matches[0].substring(
+                matches[0].indexOf("{{") + 2,
+                matches[0].indexOf("}}"),
+            );
+            break;
+        }
+        const currentSectionEnd = matches.index + matches[0].length;
+        alreadyCheckedText = alreadyCheckedText.concat(
+            remainingText.substring(0, currentSectionEnd),
+        );
+
+        remainingText = remainingText.substring(currentSectionEnd);
+    } while (remainingText.length > 0);
+
+    return variableName;
 }
