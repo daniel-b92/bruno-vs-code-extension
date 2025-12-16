42e0c2bea528ea56f0d1fbf24f7e3722f5dec39e
daniel-b92 <danielbeicht@web.de>
2025-12-16 13:48:08 +0100

    Also show hovers for template strings without substitution

--- src/languageFeatures/internal/brunoFiles/shared/codeBlocksUtils/getParameterNameForRequest.ts
+++ src/languageFeatures/internal/brunoFiles/shared/codeBlocksUtils/getParameterNameForRequest.ts
@@ -78,7 +78,12 @@ export function getParameterNameForRequest({
         ) {
             const resultNode = childContainingPosition
                 .getChildren(sourceFile)
-                .find((child) => child.kind == SyntaxKind.StringLiteral);
+                .find((child) =>
+                    [
+                        SyntaxKind.NoSubstitutionTemplateLiteral,
+                        SyntaxKind.StringLiteral,
+                    ].includes(child.kind),
+                );
 
             return resultNode?.getText(sourceFile);
         }
