5a0e8e60025b629e6e748bf9a69fd331b53cf503
daniel-b92 <danielbeicht@web.de>
2025-12-23 13:20:52 +0100

    move utility function for parsing vars from non code blocks into separate file

--- src/shared/fileParsing/internal/getBlockContent.ts
+++ src/shared/fileParsing/internal/getBlockContent.ts
@@ -10,15 +10,13 @@ import {
     DictionaryBlockSimpleField,
     PlainTextWithinBlock,
     BrunoVariableReference,
-    getPatternForVariablesInNonCodeBlock,
-    VariableReferenceType,
-    BrunoVariableType,
 } from "../..";
 import { parseJsonBlock } from "./parseJsonBlock";
 import { parsePlainTextBlock } from "./parsePlainTextBlock";
 import { parseDictionaryBlock } from "./parseDictionaryBlock";
 import { parseArrayBlock } from "./parseArrayBlock";
 import { SyntaxKind } from "typescript";
+import { getBrunoVariableReferencesInNonCodeBlock } from "./getBrunoVariableReferencesInNonCodeBlock";
 
 export const getBlockContent = (
     document: TextDocumentHelper,
@@ -105,58 +103,3 @@ export const getBlockContent = (
             );
     }
 };
-
-function getBrunoVariableReferencesInNonCodeBlock(
-    fullDocumentHelper: TextDocumentHelper,
-    contentRange: Range,
-): BrunoVariableReference[] {
-    const result: BrunoVariableReference[] = [];
-    let remainingContent = fullDocumentHelper.getText(contentRange);
-
-    do {
-        const matches =
-            getPatternForVariablesInNonCodeBlock().exec(remainingContent);
-
-        if (matches == null || matches.length == 0) {
-            return result;
-        }
-        const matchingText = matches[0];
-        const variableStartOffsetWithinMatch = 2;
-        const variableName = matchingText.substring(
-            matchingText.indexOf("{{") + variableStartOffsetWithinMatch,
-            matchingText.indexOf("}}"),
-        );
-        const variableStartPositionInFullDocument =
-            fullDocumentHelper.getPositionForOffset(
-                contentRange.start,
-                matches.index + variableStartOffsetWithinMatch,
-            );
-
-        if (!variableStartPositionInFullDocument) {
-            return result;
-        }
-
-        result.push({
-            variableName,
-            variableNameRange: new Range(
-                variableStartPositionInFullDocument,
-                new Position(
-                    variableStartPositionInFullDocument.line,
-                    variableStartPositionInFullDocument.character +
-                        variableName.length,
-                ),
-            ),
-            referenceType: VariableReferenceType.Read,
-            variableType: BrunoVariableType.Unknown,
-        });
-
-        remainingContent =
-            remainingContent.length > matches.index + matchingText.length
-                ? remainingContent.substring(
-                      matches.index + matchingText.length,
-                  )
-                : "";
-    } while (remainingContent.length > 0);
-
-    return result;
-}
---
+++ src/shared/fileParsing/internal/getBrunoVariableReferencesInNonCodeBlock.ts
@@ -0,0 +1,64 @@
+import {
+    BrunoVariableReference,
+    BrunoVariableType,
+    getPatternForVariablesInNonCodeBlock,
+    Position,
+    Range,
+    TextDocumentHelper,
+    VariableReferenceType,
+} from "../..";
+
+export function getBrunoVariableReferencesInNonCodeBlock(
+    fullDocumentHelper: TextDocumentHelper,
+    contentRange: Range,
+): BrunoVariableReference[] {
+    const result: BrunoVariableReference[] = [];
+    let remainingContent = fullDocumentHelper.getText(contentRange);
+
+    do {
+        const matches =
+            getPatternForVariablesInNonCodeBlock().exec(remainingContent);
+
+        if (matches == null || matches.length == 0) {
+            return result;
+        }
+        const matchingText = matches[0];
+        const variableStartOffsetWithinMatch = 2;
+        const variableName = matchingText.substring(
+            matchingText.indexOf("{{") + variableStartOffsetWithinMatch,
+            matchingText.indexOf("}}"),
+        );
+        const variableStartPositionInFullDocument =
+            fullDocumentHelper.getPositionForOffset(
+                contentRange.start,
+                matches.index + variableStartOffsetWithinMatch,
+            );
+
+        if (!variableStartPositionInFullDocument) {
+            return result;
+        }
+
+        result.push({
+            variableName,
+            variableNameRange: new Range(
+                variableStartPositionInFullDocument,
+                new Position(
+                    variableStartPositionInFullDocument.line,
+                    variableStartPositionInFullDocument.character +
+                        variableName.length,
+                ),
+            ),
+            referenceType: VariableReferenceType.Read,
+            variableType: BrunoVariableType.Unknown,
+        });
+
+        remainingContent =
+            remainingContent.length > matches.index + matchingText.length
+                ? remainingContent.substring(
+                      matches.index + matchingText.length,
+                  )
+                : "";
+    } while (remainingContent.length > 0);
+
+    return result;
+}
