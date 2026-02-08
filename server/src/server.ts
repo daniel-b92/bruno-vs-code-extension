a08554458bf2f74097f92378d6b8bf7b58508abb
daniel-b92 <danielbeicht@web.de>
2026-02-08 19:59:45 +0100

    add file system cache refresh for server after initialization

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,9 @@ import {
 
 import { TextDocument } from "vscode-languageserver-textdocument";
 import { getHandlerForFormatting } from "./bruFiles/formatting/getHandlerForFormatting";
+import { HelperProvider } from "./shared";
+
+let helperProvider: HelperProvider;
 
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -16,15 +19,25 @@ const connection = createConnection(ProposedFeatures.all);
 // Create a simple text document manager.
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
-connection.onInitialize(() => {
+connection.onInitialize(async () => {
     const result: InitializeResult = {
         capabilities: {
+            workspace: { workspaceFolders: { supported: true } },
             textDocumentSync: TextDocumentSyncKind.Full,
-            // Tell the client that this server supports code completion.
             documentFormattingProvider: true,
         },
     };
     return result;
+});
+
+connection.onInitialized(async () => {
+    const workspaceFolders = await getWorkspaceFolders();
+    helperProvider = new HelperProvider(workspaceFolders);
+
+    connection.console.info("Starting to refresh cache...");
+    await helperProvider.getItemProvider().refreshCache(workspaceFolders);
+
+    connection.console.info("Done refreshing cache.");
 });
 
 connection.onDocumentFormatting(({ textDocument: { uri } }) => {
@@ -38,3 +51,11 @@ documents.listen(connection);
 
 // Listen on the connection
 connection.listen();
+
+async function getWorkspaceFolders() {
+    return (
+        (await connection.workspace.getWorkspaceFolders())?.map(
+            (f) => f.name,
+        ) ?? []
+    );
+}
---
+++ server/src/shared/helperProvider.ts
@@ -0,0 +1,31 @@
+import {
+    FileChangedEvent,
+    CollectionWatcher,
+    CollectionItemProvider,
+} from "@global_shared";
+import { Evt } from "evt";
+import { AdditionalCollectionData, TypedCollectionItemProvider } from ".";
+
+export class HelperProvider {
+    constructor(workspaceFolders: string[]) {
+        this.fileChangedEmitter = Evt.create<FileChangedEvent>();
+        this.collectionWatcher = new CollectionWatcher(
+            this.fileChangedEmitter,
+            workspaceFolders,
+        );
+        this.itemProvider =
+            new CollectionItemProvider<AdditionalCollectionData>(
+                this.collectionWatcher,
+                () => {},
+                [],
+            );
+    }
+
+    private fileChangedEmitter: Evt<FileChangedEvent>;
+    private collectionWatcher: CollectionWatcher;
+    private itemProvider: TypedCollectionItemProvider;
+
+    public getItemProvider() {
+        return this.itemProvider;
+    }
+}
