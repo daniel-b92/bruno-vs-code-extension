36a1cba0bd3f3251e68ef8c69e18cfc131310f32
daniel-b92 <danielbeicht@web.de>
2026-02-22 14:48:53 +0100

    remove some unused functions/interfaces from client project

--- client/src/languageFeatures/internal/shared/interfaces.ts
+++ client/src/languageFeatures/internal/shared/interfaces.ts
@@ -3,30 +3,9 @@ import {
     Position as VsCodePosition,
     CancellationToken,
 } from "vscode";
-import { Block, VariableReferenceType } from "@global_shared";
-import { OutputChannelLogger, TypedCollection } from "@shared";
 
 export interface LanguageFeatureRequest {
     document: TextDocument;
     position: VsCodePosition;
     token: CancellationToken;
-}
-
-export interface EnvVariableRequest {
-    requestData: EnvVariableCommonRequestData;
-    bruFileSpecificData?: EnvVariableBruFileSpecificData;
-    logger?: OutputChannelLogger;
-}
-
-export interface EnvVariableCommonRequestData {
-    collection: TypedCollection;
-    variableName: string;
-    functionType: VariableReferenceType;
-    requestPosition: VsCodePosition;
-    token: CancellationToken;
-}
-
-export interface EnvVariableBruFileSpecificData {
-    blockContainingPosition: Block;
-    allBlocks: Block[];
 }
