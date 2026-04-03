8a901ea2858dea6a612dae5de38ac56c2102f397
daniel-b92 <danielbeicht@web.de>
2026-04-03 16:32:16 +0200

    use new global shared setting getter for fetching setting in server as well

--- server/src/jsFiles/completionItems/handleCompletionRequest.ts
+++ server/src/jsFiles/completionItems/handleCompletionRequest.ts
@@ -19,18 +19,13 @@ import { CompletionItem } from "vscode-languageserver";
 
 export async function handleCompletionRequest({
     baseRequest,
-    itemProvider,
+    collection,
     configuredEnvironmentName,
     logger,
 }: LanguageRequestWithTestEnvironmentInfo): Promise<
     CompletionItem[] | undefined
 > {
-    const { token, filePath } = baseRequest;
-    const collection = itemProvider.getAncestorCollectionForPath(filePath);
-
-    if (!collection) {
-        return [];
-    }
+    const { token } = baseRequest;
 
     if (token.isCancellationRequested) {
         addLogEntryForCancellation(logger);
