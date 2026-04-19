b58b008b32511681a8f5fc8f33b7e07117a76037
daniel-b92 <danielbeicht@web.de>
2026-04-19 21:06:51 +0200

    move utility functions to own files for better reusability

--- server/src/bruFiles/completions/dictionaryBlocks/specificBlocks/getMethodBlockContentCompletions.ts
+++ server/src/bruFiles/completions/dictionaryBlocks/specificBlocks/getMethodBlockContentCompletions.ts
@@ -4,16 +4,12 @@ import {
     MethodBlockAuthValues,
     Block,
     getMandatoryKeysForMethodBlock,
-    Range,
-    Position,
-    TextDocumentHelper,
     isAuthBlock,
     AuthBlockName,
     OAuth2AuthBlocksCommonKeys,
     getMandatoryKeysForNonOAuth2Block,
     AuthBlockNamesExcludingOAuth2,
     LineBreakType,
-    BlockBracket,
     getExpectedAuthBlockForType,
     isBodyBlock,
     getAuthTypesForNoDefinedAuthBlock,
@@ -24,7 +20,9 @@ import { getLinePatternForDictionaryField } from "../generic/getLinePatternForDi
 
 import { getCompletionsForKeys } from "../generic/getCompletionsForKeys";
 import { getTextEditForDictionaryBlockSimpleValue } from "../generic/getTextEditForDictionaryBlockSimpleValue";
-import { CompletionItem, TextEdit } from "vscode-languageserver";
+import { CompletionItem } from "vscode-languageserver";
+import { getTextEditForInsertingBlock } from "../../../shared/getTextEditForInsertingBlock";
+import { getTextEditForRemovingBlock } from "../../../shared/getTextEditForRemovingBlock";
 
 export function getMethodBlockContentCompletions(
     request: LanguageFeatureBaseRequest,
@@ -221,103 +219,4 @@ function getCompletionsForBodyFieldValue(
             })
             .filter((val) => val != undefined);
     }
-}
-
-function getTextEditForRemovingBlock(
-    docHelper: TextDocumentHelper,
-    block: Block,
-): TextEdit | undefined {
-    const firstBlockLine = block.nameRange.start.line;
-    const blankLinePattern = /^\s*$/m;
-
-    const fullBlockRange = getFullBlockRange(docHelper, block);
-    if (fullBlockRange == undefined) {
-        return undefined;
-    }
-
-    // Remove empty lines between block and previous block, too.
-    const firstLineToRemove = docHelper
-        .getAllLines()
-        .filter(({ index }) => index <= firstBlockLine)
-        .reverse()
-        .find(({ content, index }) => {
-            if (index == 0) {
-                return true;
-            }
-
-            const previousLineContent = docHelper.getLineByIndex(index - 1);
-
-            return (
-                (blankLinePattern.test(content) || index == firstBlockLine) &&
-                !blankLinePattern.test(previousLineContent)
-            );
-        });
-
-    if (!firstLineToRemove) {
-        return { newText: "", range: fullBlockRange };
-    }
-
-    const rangeStart =
-        firstLineToRemove.index == 0
-            ? new Position(0, 0)
-            : (docHelper.getRangeForLine(firstLineToRemove.index - 1)?.end ??
-              new Position(firstLineToRemove.index, 0));
-
-    return {
-        newText: "",
-        range: new Range(rangeStart, fullBlockRange.end),
-    };
-}
-
-function getTextEditForInsertingBlock(
-    methodBlock: Block,
-    newData: {
-        blockName: string;
-        content?: { keys: string[] };
-        lineBreak: LineBreakType;
-    },
-): TextEdit {
-    const { blockName: newBlockName, content, lineBreak } = newData;
-
-    const blockStartLine = `${newBlockName} ${BlockBracket.OpeningBracketForDictionaryOrTextBlock}`;
-    const blockEndLine = BlockBracket.ClosingBracketForDictionaryOrTextBlock;
-    const afterMethodBlockPosition = new Position(
-        // The content range does not include the closing block bracket.
-        methodBlock.contentRange.end.line + 1,
-        0,
-    );
-    const textEditRange = new Range(
-        afterMethodBlockPosition,
-        afterMethodBlockPosition,
-    );
-
-    return {
-        newText: lineBreak.concat(
-            blockStartLine,
-            lineBreak,
-            content
-                ? content.keys.map((key) => `\t${key}:`).join(lineBreak)
-                : "\t",
-            lineBreak,
-            blockEndLine,
-            lineBreak,
-        ),
-        range: textEditRange,
-    };
-}
-
-function getFullBlockRange(
-    docHelper: TextDocumentHelper,
-    blockToReplace: Block,
-) {
-    const blockStartLine = blockToReplace.nameRange.start.line;
-    const blockEndLine = blockToReplace.contentRange.end.line;
-    const fullBlockEnd = docHelper.getRangeForLine(blockEndLine)?.end;
-
-    return fullBlockEnd
-        ? new Range(
-              new Position(blockStartLine, 0),
-              new Position(blockEndLine, fullBlockEnd.character),
-          )
-        : undefined;
 }
