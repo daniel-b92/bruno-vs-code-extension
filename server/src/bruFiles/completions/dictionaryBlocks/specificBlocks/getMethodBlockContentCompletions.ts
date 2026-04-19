42caa4bac4ced7061cad00f3125259ed8db1ccac
daniel-b92 <danielbeicht@web.de>
2026-04-19 21:00:49 +0200

    finalize new additional text edits

--- server/src/bruFiles/completions/dictionaryBlocks/specificBlocks/getMethodBlockContentCompletions.ts
+++ server/src/bruFiles/completions/dictionaryBlocks/specificBlocks/getMethodBlockContentCompletions.ts
@@ -7,7 +7,6 @@ import {
     Range,
     Position,
     TextDocumentHelper,
-    RequestFileBlockName,
     isAuthBlock,
     AuthBlockName,
     OAuth2AuthBlocksCommonKeys,
@@ -17,22 +16,19 @@ import {
     BlockBracket,
     getExpectedAuthBlockForType,
     isBodyBlock,
+    getAuthTypesForNoDefinedAuthBlock,
+    getBodyBlockTypeForNoDefinedBodyBlock,
 } from "@global_shared";
 import { LanguageFeatureBaseRequest } from "../../../../shared";
-import { getFixedCompletionItems } from "../generic/getFixedCompletionItems";
 import { getLinePatternForDictionaryField } from "../generic/getLinePatternForDictionaryField";
 
 import { getCompletionsForKeys } from "../generic/getCompletionsForKeys";
 import { getTextEditForDictionaryBlockSimpleValue } from "../generic/getTextEditForDictionaryBlockSimpleValue";
 import { CompletionItem, TextEdit } from "vscode-languageserver";
 
-const fieldType = {
-    Auth: MethodBlockKey.Auth,
-    Body: MethodBlockKey.Body,
-} as const;
-
 export function getMethodBlockContentCompletions(
     request: LanguageFeatureBaseRequest,
+    allBlocks: Block[],
     block: Block,
 ) {
     const completionsForKeys = getCompletionsForKeys(request, block, {
@@ -43,148 +39,271 @@ export function getMethodBlockContentCompletions(
         return completionsForKeys;
     }
 
-    return getFixedCompletionItems(
-        [
-            {
-                linePattern: getLinePatternForDictionaryField(
-                    MethodBlockKey.Body,
-                ),
-                choices: Object.values(MethodBlockBodies),
-            },
-            {
-                linePattern: getLinePatternForDictionaryField(
-                    MethodBlockKey.Auth,
-                ),
-                choices: Object.values(MethodBlockAuthValues),
-            },
-        ],
+    return getValueCompletions(
+        block,
+        allBlocks,
         request,
+        request.documentHelper.getMostUsedLineBreak() ?? LineBreakType.Lf,
     );
 }
 
-export function getValueCompletions(
+function getValueCompletions(
     methodBlock: Block,
     allBlocks: Block[],
-    { documentHelper, position: { line } }: LanguageFeatureBaseRequest,
+    baseRequest: LanguageFeatureBaseRequest,
     lineBreak: LineBreakType,
-): CompletionItem[] {
+): CompletionItem[] | undefined {
+    const {
+        documentHelper,
+        position: { line },
+    } = baseRequest;
     const currentText = documentHelper.getLineByIndex(line);
 
-    const isInAuthField =
-        currentText.match(getLinePatternForDictionaryField(MethodBlockKey.Auth))
-            ?.length ?? -1 > 0;
+    const isInAuthField = getLinePatternForDictionaryField(
+        MethodBlockKey.Auth,
+    ).test(currentText);
+    const isInBodyField = getLinePatternForDictionaryField(
+        MethodBlockKey.Body,
+    ).test(currentText);
 
-    if (isInAuthField) {
-        const existingAuthBlocks = allBlocks.filter(({ name }) =>
-            isAuthBlock(name),
-        );
-
-        if (existingAuthBlocks.length <= 1) {
-            return Object.values(MethodBlockAuthValues).map((authType) => {
-                const newAuthBlockName = getExpectedAuthBlockForType(authType);
-                const authBlockKeys =
-                    newAuthBlockName == AuthBlockName.OAuth2Auth
-                        ? Object.values(OAuth2AuthBlocksCommonKeys)
-                        : getMandatoryKeysForNonOAuth2Block(
-                              newAuthBlockName as AuthBlockNamesExcludingOAuth2,
-                          );
-
-                return {
-                    label: authType,
-                    textEdit: getTextEditForDictionaryBlockSimpleValue(
-                        line,
-                        currentText,
-                        authType,
-                    ),
-                    additionalTextEdits: getAdditionalTextEdits(
-                        documentHelper,
-                        methodBlock,
-                        {
-                            blockName: newAuthBlockName,
-                            keys: authBlockKeys,
-                            lineBreak,
-                        },
-                    ),
-                };
-            });
-        }
+    if (!isInAuthField && !isInBodyField) {
+        return undefined;
     }
 
-    const isInBodyField =
-        currentText.match(getLinePatternForDictionaryField(MethodBlockKey.Body))
-            ?.length ?? -1 > 0;
-
-    if (isInBodyField) {
-        const existingBodyBlocks = allBlocks.filter(({ name }) =>
-            isBodyBlock(name),
+    if (isInAuthField) {
+        return getCompletionsForAuthFieldValue(
+            baseRequest,
+            allBlocks,
+            methodBlock,
+            { currentLineContent: currentText, lineBreak },
         );
+    }
 
-        if (existingBodyBlocks.length <= 1) {
-            return Object.values(MethodBlockBodies).map((bodyType) => {
-                const newBodyBlockName = `body:${bodyType}`;
+    return getCompletionsForBodyFieldValue(
+        baseRequest,
+        allBlocks,
+        methodBlock,
+        { currentLineContent: currentText, lineBreak },
+    );
+}
 
-                return {
-                    label: bodyType,
-                    textEdit: getTextEditForDictionaryBlockSimpleValue(
-                        line,
-                        currentText,
-                        bodyType,
-                    ),
-                    additionalTextEdits: getAdditionalTextEdits(
-                        documentHelper,
-                        methodBlock,
-                        {
-                            blockName: newBodyBlockName,
-                            keys: authBlockKeys,
-                            lineBreak,
-                        },
-                    ),
-                };
-            });
-        }
+function getCompletionsForAuthFieldValue(
+    { documentHelper, position: { line } }: LanguageFeatureBaseRequest,
+    allBlocks: Block[],
+    methodBlock: Block,
+    content: { currentLineContent: string; lineBreak: LineBreakType },
+) {
+    const { currentLineContent, lineBreak } = content;
+    const existingAuthBlocks = allBlocks.filter(({ name }) =>
+        isAuthBlock(name),
+    );
+    const authTypesWithoutAnAuthBlock = getAuthTypesForNoDefinedAuthBlock();
+
+    if (existingAuthBlocks.length <= 1) {
+        return Object.values(MethodBlockAuthValues)
+            .map((authType) => {
+                const label = authType;
+                const textEdit = getTextEditForDictionaryBlockSimpleValue(
+                    line,
+                    currentLineContent,
+                    authType,
+                );
+
+                if (
+                    authTypesWithoutAnAuthBlock.includes(authType) &&
+                    existingAuthBlocks.length > 0
+                ) {
+                    return {
+                        label,
+                        textEdit,
+                        additionalTextEdits: [
+                            getTextEditForRemovingBlock(
+                                documentHelper,
+                                existingAuthBlocks[0],
+                            ),
+                        ].filter((val) => val != undefined),
+                    };
+                }
+
+                if (
+                    !authTypesWithoutAnAuthBlock.includes(authType) &&
+                    existingAuthBlocks.length == 0
+                ) {
+                    const newAuthBlockName =
+                        getExpectedAuthBlockForType(authType);
+                    const authBlockKeys =
+                        newAuthBlockName == AuthBlockName.OAuth2Auth
+                            ? Object.values(OAuth2AuthBlocksCommonKeys)
+                            : getMandatoryKeysForNonOAuth2Block(
+                                  newAuthBlockName as AuthBlockNamesExcludingOAuth2,
+                              );
+
+                    return {
+                        label,
+                        textEdit,
+                        additionalTextEdits: [
+                            getTextEditForInsertingBlock(methodBlock, {
+                                blockName: newAuthBlockName,
+                                content: { keys: authBlockKeys },
+                                lineBreak,
+                            }),
+                        ].filter((val) => val != undefined),
+                    };
+                }
+
+                return { label, textEdit };
+            })
+            .filter((val) => val != undefined);
     }
 }
 
-function getAdditionalTextEdits(
+function getCompletionsForBodyFieldValue(
+    { documentHelper, position: { line } }: LanguageFeatureBaseRequest,
+    allBlocks: Block[],
+    methodBlock: Block,
+    content: { currentLineContent: string; lineBreak: LineBreakType },
+) {
+    const { currentLineContent, lineBreak } = content;
+    const existingBodyBlocks = allBlocks.filter(({ name }) =>
+        isBodyBlock(name),
+    );
+    const bodyBlockTypeWithoutABodyBlock =
+        getBodyBlockTypeForNoDefinedBodyBlock();
+
+    if (existingBodyBlocks.length <= 1) {
+        return Object.values(MethodBlockBodies)
+            .map((bodyType) => {
+                const label = bodyType;
+                const textEdit = getTextEditForDictionaryBlockSimpleValue(
+                    line,
+                    currentLineContent,
+                    bodyType,
+                );
+                if (!textEdit) {
+                    return undefined;
+                }
+
+                if (
+                    bodyType == bodyBlockTypeWithoutABodyBlock &&
+                    existingBodyBlocks.length > 0
+                ) {
+                    return {
+                        label,
+                        textEdit,
+                        additionalTextEdits: [
+                            getTextEditForRemovingBlock(
+                                documentHelper,
+                                existingBodyBlocks[0],
+                            ),
+                        ].filter((val) => val != undefined),
+                    };
+                }
+
+                if (
+                    bodyType != bodyBlockTypeWithoutABodyBlock &&
+                    existingBodyBlocks.length == 0
+                ) {
+                    const newBodyBlockName = `body:${bodyType}`;
+
+                    return {
+                        label,
+                        textEdit,
+                        additionalTextEdits: [
+                            getTextEditForInsertingBlock(methodBlock, {
+                                blockName: newBodyBlockName,
+                                lineBreak,
+                            }),
+                        ].filter((val) => val != undefined),
+                    };
+                }
+
+                return { label, textEdit };
+            })
+            .filter((val) => val != undefined);
+    }
+}
+
+function getTextEditForRemovingBlock(
     docHelper: TextDocumentHelper,
+    block: Block,
+): TextEdit | undefined {
+    const firstBlockLine = block.nameRange.start.line;
+    const blankLinePattern = /^\s*$/m;
+
+    const fullBlockRange = getFullBlockRange(docHelper, block);
+    if (fullBlockRange == undefined) {
+        return undefined;
+    }
+
+    // Remove empty lines between block and previous block, too.
+    const firstLineToRemove = docHelper
+        .getAllLines()
+        .filter(({ index }) => index <= firstBlockLine)
+        .reverse()
+        .find(({ content, index }) => {
+            if (index == 0) {
+                return true;
+            }
+
+            const previousLineContent = docHelper.getLineByIndex(index - 1);
+
+            return (
+                (blankLinePattern.test(content) || index == firstBlockLine) &&
+                !blankLinePattern.test(previousLineContent)
+            );
+        });
+
+    if (!firstLineToRemove) {
+        return { newText: "", range: fullBlockRange };
+    }
+
+    const rangeStart =
+        firstLineToRemove.index == 0
+            ? new Position(0, 0)
+            : (docHelper.getRangeForLine(firstLineToRemove.index - 1)?.end ??
+              new Position(firstLineToRemove.index, 0));
+
+    return {
+        newText: "",
+        range: new Range(rangeStart, fullBlockRange.end),
+    };
+}
+
+function getTextEditForInsertingBlock(
     methodBlock: Block,
     newData: {
         blockName: string;
-        keys: string[];
+        content?: { keys: string[] };
         lineBreak: LineBreakType;
     },
-    blockToReplace?: Block,
-): TextEdit[] | undefined {
-    const { blockName: newBlockName, keys: newKeys, lineBreak } = newData;
+): TextEdit {
+    const { blockName: newBlockName, content, lineBreak } = newData;
 
-    const blockStartLine = `${newBlockName} ${BlockBracket.OpeningBracketForDictionaryOrTextBlock}${lineBreak}`;
+    const blockStartLine = `${newBlockName} ${BlockBracket.OpeningBracketForDictionaryOrTextBlock}`;
     const blockEndLine = BlockBracket.ClosingBracketForDictionaryOrTextBlock;
-    const commonText = blockStartLine.concat(
-        lineBreak,
-        newKeys.map((key) => `\t${key}:`).join(lineBreak),
-        lineBreak,
-        blockEndLine,
-    );
-
-    if (blockToReplace != undefined) {
-        const range = getFullBlockRange(docHelper, blockToReplace);
-        return range ? [{ newText: commonText, range }] : undefined;
-    }
-
     const afterMethodBlockPosition = new Position(
-        methodBlock.contentRange.end.line,
+        // The content range does not include the closing block bracket.
+        methodBlock.contentRange.end.line + 1,
         0,
     );
+    const textEditRange = new Range(
+        afterMethodBlockPosition,
+        afterMethodBlockPosition,
+    );
 
-    return [
-        {
-            newText: lineBreak.concat(commonText, lineBreak),
-            range: new Range(
-                afterMethodBlockPosition,
-                afterMethodBlockPosition,
-            ),
-        },
-    ];
+    return {
+        newText: lineBreak.concat(
+            blockStartLine,
+            lineBreak,
+            content
+                ? content.keys.map((key) => `\t${key}:`).join(lineBreak)
+                : "\t",
+            lineBreak,
+            blockEndLine,
+            lineBreak,
+        ),
+        range: textEditRange,
+    };
 }
 
 function getFullBlockRange(
