b8d2c78933409f0ddd87435c044f3466afbaa1f6
daniel-b92 <danielbeicht@web.de>
2026-04-26 18:57:43 +0200

    Add option for unselecting environment in explorer (#139)
    * add missing option for unselecting an environment for a collection
    
    * display selected environment correctly in hovers
    
    * bump patch version and finalize changelog entry

--- client/src/treeView/internal/explorer/showDialogForSettingEnvironment.ts
+++ client/src/treeView/internal/explorer/showDialogForSettingEnvironment.ts
@@ -1,5 +1,9 @@
 import { getExtensionForBrunoFiles } from "@global_shared";
-import { TypedCollection, updateSettings } from "@shared";
+import {
+    removeConfigForCollection,
+    TypedCollection,
+    updateSettings,
+} from "@shared";
 import { basename } from "path";
 import { QuickPickItem, window } from "vscode";
 
@@ -14,10 +18,19 @@ export async function showDialogForSettingEnvironment(
             selected: configuredEnvironmentName === environmentName,
         }));
 
-    const options: QuickPickItem[] = environments.map(({ item, selected }) => ({
-        label: basename(item.getPath(), getExtensionForBrunoFiles()),
-        description: selected ? "Selected" : undefined,
-    }));
+    const labelForNoSelectedEnvironment = "None";
+
+    const options: QuickPickItem[] = (
+        environments.map(({ item, selected }) => ({
+            label: basename(item.getPath(), getExtensionForBrunoFiles()),
+            description: selected ? "Selected" : undefined,
+        })) as QuickPickItem[]
+    ).concat({
+        label: labelForNoSelectedEnvironment,
+        description:
+            configuredEnvironmentName == undefined ? "Selected" : undefined,
+        detail: "Option for not selecting an environment",
+    });
 
     const selectedOption = await window.showQuickPick(options, {
         title: `Environment for collection '${basename(collection.getRootDirectory())}'`,
@@ -25,6 +38,11 @@ export async function showDialogForSettingEnvironment(
 
     if (!selectedOption) {
         return false;
+    }
+
+    if (selectedOption.label == labelForNoSelectedEnvironment) {
+        await removeConfigForCollection(collection.getRootDirectory());
+        return true;
     }
 
     await updateSettings(collection.getRootDirectory(), selectedOption.label);
