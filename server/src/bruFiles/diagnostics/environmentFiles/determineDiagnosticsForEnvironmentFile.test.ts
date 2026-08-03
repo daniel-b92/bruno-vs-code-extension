2ea7f3e4c0f98e7cb4df6f8774c86d106b0016cb
daniel-b92 <danielbeicht@web.de>
2026-08-03 18:05:48 +0200

    improve existing and add second test for annotation check

--- server/src/bruFiles/diagnostics/environmentFiles/determineDiagnosticsForEnvironmentFile.test.ts
+++ server/src/bruFiles/diagnostics/environmentFiles/determineDiagnosticsForEnvironmentFile.test.ts
@@ -3,13 +3,15 @@ import {
     BrunoFileType,
     parseBruFile,
     TextDocumentHelper,
-} from "../../../../../shared/index";
+} from "@global_shared";
 import { determineDiagnosticsForEnvironmentFile } from "./determineDiagnosticsForEnvironmentFile";
+import { NonBlockSpecificDiagnosticCode } from "../shared/diagnosticCodes/nonBlockSpecificDiagnosticCodeEnum";
 
 describe("determineDiagnosticsForEnvironmentFile", () => {
     it("parses a simple environment file string and returns diagnostics for invalid annotations", () => {
         const documentText = `vars {
   @description("first")
+  @number
   @description("duplicate")
   first: 1
 }`;
@@ -29,10 +31,36 @@ describe("determineDiagnosticsForEnvironmentFile", () => {
 
         expect(diagnostics).toHaveLength(2);
         expect(diagnostics.map((diagnostic) => diagnostic.code).sort()).toEqual(
-            ["bru30", "bru30"],
+            [
+                NonBlockSpecificDiagnosticCode.DuplicateAnnotationOfSameSortInDictionaryBlock,
+                NonBlockSpecificDiagnosticCode.DuplicateAnnotationOfSameSortInDictionaryBlock,
+            ],
         );
         expect(diagnostics.map(({ range }) => range.start.line).sort()).toEqual(
-            [1, 2],
+            [1, 3],
         );
+    });
+
+    it("parses a simple environment file string and does not return diagnostic for valid annotations", () => {
+        const documentText = `vars {
+  @description("first")
+  @number
+  first: 1
+}`;
+        const document = new TextDocumentHelper(documentText);
+        const { blocks } = parseBruFile(
+            document,
+            BrunoFileType.EnvironmentFile,
+        );
+
+        expect(blocks).toHaveLength(1);
+        expect(blocks[0].name).toBe("vars");
+
+        const diagnostics = determineDiagnosticsForEnvironmentFile(
+            "/tmp/collection/environments/invalid.bru",
+            documentText,
+        );
+
+        expect(diagnostics).toHaveLength(0);
     });
 });
