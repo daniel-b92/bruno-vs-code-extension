195e79aa8717b000d207e596e9d3e1545854b995
daniel-b92 <danielbeicht@web.de>
2026-08-04 18:51:19 +0200

    fix parsing and remove temporary comments

--- shared/fileParsing/external/yamlFormat/parseYamlEnvironmentFile.ts
+++ shared/fileParsing/external/yamlFormat/parseYamlEnvironmentFile.ts
@@ -1,14 +1,11 @@
 import {
-    Alias,
-    Document,
     isMap,
-    isScalar,
+    isSeq,
     LineCounter,
-    ParsedNode,
     parseDocument,
-    Scalar,
     YAMLError,
     YAMLMap,
+    Range as YamlRange,
     YAMLSeq,
 } from "yaml";
 import { Position, Range, TextDocumentHelper } from "../../..";
@@ -18,89 +15,181 @@ enum EnvironmentTopLevelKey {
     Variables = "variables",
 }
 
-enum YamlNodeTypes {
-    Pair = 1,
-    Sequence = 2,
-}
-
-type ParsedDocument =
-    | Document.Parsed<Alias.Parsed, true>
-    | Document.Parsed<Scalar.Parsed, true>
-    | Document.Parsed<YAMLMap.Parsed<ParsedNode, ParsedNode | null>, true>
-    | Document.Parsed<YAMLSeq.Parsed<ParsedNode>, true>;
-
 export interface YamlParsingError {
     message: string;
     range: Range;
     severity: "ERR" | "WARN";
 }
 
+interface CommonParsingArgs {
+    docHelper: TextDocumentHelper;
+    fullDocumentRange: Range;
+}
+
 export function parseYamlEnvironmentFile(docHelper: TextDocumentHelper) {
     const document = parseDocument(docHelper.getText(), {
         lineCounter: new LineCounter(),
     });
+    const fullDocumentRange = docHelper.getTextRange();
 
     if (document.errors.length > 0) {
         console.log("Got technical parsing errors.");
-        return mapErrors(document.errors, docHelper.getTextRange());
+        return mapErrors(document.errors, fullDocumentRange);
     }
 
-    const mandatoryTopLevelFieldErrors =
-        checkMandatoryTopLevelFieldsExistAndHaveCorrectStructure(
-            document,
-            [
-                {
-                    key: EnvironmentTopLevelKey.Name,
-                    structure: YamlNodeTypes.Pair,
-                },
-                {
-                    key: EnvironmentTopLevelKey.Variables,
-                    structure: YamlNodeTypes.Sequence,
-                },
-            ],
-            docHelper.getTextRange(),
-        );
+    const maybeTopLevelMap = getTopLevelMapIfExists({
+        node: document.contents,
+        fullDocumentRange,
+        docHelper,
+    });
+    if ("error" in maybeTopLevelMap) {
+        console.log("Got errors for top level map typeguard.");
+        return maybeTopLevelMap.error;
+    }
+    const { map: topLevelMap } = maybeTopLevelMap;
 
-    if (mandatoryTopLevelFieldErrors.length > 0) {
-        console.log("Got mandatory field errors.");
-        return mandatoryTopLevelFieldErrors;
+    const maybeNameField = getScalarFieldStringValueFromMap({
+        map: topLevelMap,
+        key: EnvironmentTopLevelKey.Name,
+        docHelper,
+        fullDocumentRange,
+        isTopLevelMap: true,
+    });
+
+    if ("error" in maybeNameField) {
+        return maybeNameField.error;
     }
 
-    return document.contents;
+    const maybeVariablesSequence = getYamlSequenceByKey({
+        map: topLevelMap,
+        key: EnvironmentTopLevelKey.Variables,
+        docHelper,
+        fullDocumentRange,
+        isTopLevelMap: true,
+    });
+
+    if ("error" in maybeVariablesSequence) {
+        return maybeVariablesSequence.error;
+    }
+
+    return {
+        name: maybeNameField.value,
+        variables: maybeVariablesSequence.sequence,
+    };
 }
 
-function checkMandatoryTopLevelFieldsExistAndHaveCorrectStructure(
-    document: ParsedDocument,
-    expected: {
+function getTopLevelMapIfExists({
+    node,
+    fullDocumentRange,
+}: CommonParsingArgs & { node: unknown }):
+    { map: YAMLMap<unknown, unknown> } | { error: YamlParsingError } {
+    return isMap(node)
+        ? { map: node }
+        : {
+              error: {
+                  message: `A top level Yaml map is required`,
+                  range: fullDocumentRange,
+                  severity: "ERR" as "ERR",
+              },
+          };
+}
+
+function getScalarFieldStringValueFromMap(
+    args: CommonParsingArgs & {
+        map: YAMLMap<unknown, unknown>;
         key: string;
-        structure: YamlNodeTypes;
-    }[],
-    fullDocumentRange: Range,
-): YamlParsingError[] {
-    return expected
-        .map(({ key, structure }) =>
-            !document.has(key) ||
-            !runTypeguardCheckForStructure(document.get(key), structure)
-                ? undefined
-                : {
-                      message: `Key '${key}' is either missing or corrsponding does not have the correct structure`,
-                      range: fullDocumentRange,
-                      severity: "ERR" as "ERR",
-                  },
-        )
-        .filter((val) => val != undefined);
+        isTopLevelMap: boolean;
+    },
+): { value: string } | { error: YamlParsingError } {
+    const { map, key, fullDocumentRange, docHelper, isTopLevelMap } = args;
+
+    const existenceError = validateKeyExistsInMap(args);
+    if (existenceError) {
+        return { error: existenceError };
+    }
+
+    const field = map.get(key);
+    return typeof field == "string"
+        ? { value: field }
+        : {
+              error: {
+                  message: `Field '${key}' should be a string.`,
+                  range: isTopLevelMap
+                      ? fullDocumentRange
+                      : ((map.range
+                            ? fromYamlRange(map.range, docHelper)
+                            : fullDocumentRange) ?? fullDocumentRange),
+                  severity: "ERR" as "ERR",
+              },
+          };
 }
 
-function runTypeguardCheckForStructure(
-    node: unknown,
-    structure: YamlNodeTypes,
-) {
-    switch (structure) {
-        case YamlNodeTypes.Pair:
-            return isScalar(node);
-        case YamlNodeTypes.Sequence:
-            return isMap(node);
+function getYamlSequenceByKey(
+    args: CommonParsingArgs & {
+        map: YAMLMap<unknown, unknown>;
+        key: string;
+        isTopLevelMap: boolean;
+    },
+): { sequence: YAMLSeq<unknown> } | { error: YamlParsingError } {
+    const { map: parentMap, key, fullDocumentRange, docHelper } = args;
+
+    const existenceError = validateKeyExistsInMap(args);
+    if (existenceError) {
+        return { error: existenceError };
     }
+
+    const field = parentMap.get(key);
+    return isSeq(field)
+        ? { sequence: field }
+        : {
+              error: {
+                  message: `Field '${key}' should be a Yaml sequence. Got ${JSON.stringify(field)}`,
+                  range:
+                      (parentMap.range
+                          ? fromYamlRange(parentMap.range, docHelper)
+                          : fullDocumentRange) ?? fullDocumentRange,
+                  severity: "ERR" as "ERR",
+              },
+          };
+}
+
+function validateKeyExistsInMap(
+    args: CommonParsingArgs & {
+        map: YAMLMap<unknown, unknown>;
+        key: string;
+        isTopLevelMap: boolean;
+    },
+): YamlParsingError | undefined {
+    const { map, key, isTopLevelMap, fullDocumentRange, docHelper } = args;
+    return map.has(key)
+        ? undefined
+        : {
+              message: `Mandatory key '${key}' is missing in Yaml map.`,
+              range: isTopLevelMap
+                  ? fullDocumentRange
+                  : ((map.range
+                        ? fromYamlRange(map.range, docHelper)
+                        : fullDocumentRange) ?? fullDocumentRange),
+              severity: "ERR" as "ERR",
+          };
+}
+
+function fromYamlRange(
+    { "0": startOffset, "2": endOffset }: YamlRange,
+    docHelper: TextDocumentHelper,
+): Range | undefined {
+    const startPosition = docHelper.getPositionForOffset(
+        new Position(0, 0),
+        startOffset,
+    );
+    const endPosition = docHelper.getPositionForOffset(
+        new Position(0, 0),
+        endOffset,
+    );
+
+    return startPosition && endPosition
+        ? new Range(startPosition, endPosition)
+        : undefined;
 }
 
 function mapErrors(
