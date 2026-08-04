0511214e81c8807012a9051d99f5570f815e9693
daniel-b92 <danielbeicht@web.de>
2026-08-04 13:01:17 +0200

    add first few checks for yaml environment files

--- shared/fileParsing/external/yamlFormat/parseYamlEnvironmentFile.test.ts
+++ shared/fileParsing/external/yamlFormat/parseYamlEnvironmentFile.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from "@jest/globals";
 import { parseYamlEnvironmentFile } from "./parseYamlEnvironmentFile";
+import { TextDocumentHelper } from "../../../fileSystem/textDocumentHelper";
 
 describe("parseYamlEnvironmentFile", () => {
     it("parses a simple yaml environment file string", () => {
@@ -8,7 +9,11 @@ variables:
   - name: var-1
     value: test-1
     description: desc`;
-        const parsed = parseYamlEnvironmentFile(documentText);
+        const parsed = parseYamlEnvironmentFile(
+            new TextDocumentHelper(documentText),
+        );
+
+        console.log(JSON.stringify(parsed, null, 2));
 
         expect(parsed).toEqual({
             name: "Env1",
--- shared/fileParsing/external/yamlFormat/parseYamlEnvironmentFile.ts
+++ shared/fileParsing/external/yamlFormat/parseYamlEnvironmentFile.ts
@@ -1,7 +1,128 @@
-import { LineCounter, parseDocument } from "yaml";
+import {
+    Alias,
+    Document,
+    isMap,
+    isScalar,
+    LineCounter,
+    ParsedNode,
+    parseDocument,
+    Scalar,
+    YAMLError,
+    YAMLMap,
+    YAMLSeq,
+} from "yaml";
+import { Position, Range, TextDocumentHelper } from "../../..";
 
-export function parseYamlEnvironmentFile(content: string) {
-    const document = parseDocument(content, { lineCounter: new LineCounter() });
+enum EnvironmentTopLevelKey {
+    Name = "name",
+    Variables = "variables",
+}
 
-    return document.toJS();
+enum YamlNodeTypes {
+    Pair = 1,
+    Sequence = 2,
+}
+
+type ParsedDocument =
+    | Document.Parsed<Alias.Parsed, true>
+    | Document.Parsed<Scalar.Parsed, true>
+    | Document.Parsed<YAMLMap.Parsed<ParsedNode, ParsedNode | null>, true>
+    | Document.Parsed<YAMLSeq.Parsed<ParsedNode>, true>;
+
+export interface YamlParsingError {
+    message: string;
+    range: Range;
+    severity: "ERR" | "WARN";
+}
+
+export function parseYamlEnvironmentFile(docHelper: TextDocumentHelper) {
+    const document = parseDocument(docHelper.getText(), {
+        lineCounter: new LineCounter(),
+    });
+
+    if (document.errors.length > 0) {
+        console.log("Got technical parsing errors.");
+        return mapErrors(document.errors, docHelper.getTextRange());
+    }
+
+    const mandatoryTopLevelFieldErrors =
+        checkMandatoryTopLevelFieldsExistAndHaveCorrectStructure(
+            document,
+            [
+                {
+                    key: EnvironmentTopLevelKey.Name,
+                    structure: YamlNodeTypes.Pair,
+                },
+                {
+                    key: EnvironmentTopLevelKey.Variables,
+                    structure: YamlNodeTypes.Sequence,
+                },
+            ],
+            docHelper.getTextRange(),
+        );
+
+    if (mandatoryTopLevelFieldErrors.length > 0) {
+        console.log("Got mandatory field errors.");
+        return mandatoryTopLevelFieldErrors;
+    }
+
+    return document.contents;
+}
+
+function checkMandatoryTopLevelFieldsExistAndHaveCorrectStructure(
+    document: ParsedDocument,
+    expected: {
+        key: string;
+        structure: YamlNodeTypes;
+    }[],
+    fullDocumentRange: Range,
+): YamlParsingError[] {
+    return expected
+        .map(({ key, structure }) =>
+            !document.has(key) ||
+            !runTypeguardCheckForStructure(document.get(key), structure)
+                ? undefined
+                : {
+                      message: `Key '${key}' is either missing or corrsponding does not have the correct structure`,
+                      range: fullDocumentRange,
+                      severity: "ERR" as "ERR",
+                  },
+        )
+        .filter((val) => val != undefined);
+}
+
+function runTypeguardCheckForStructure(
+    node: unknown,
+    structure: YamlNodeTypes,
+) {
+    switch (structure) {
+        case YamlNodeTypes.Pair:
+            return isScalar(node);
+        case YamlNodeTypes.Sequence:
+            return isMap(node);
+    }
+}
+
+function mapErrors(
+    errors: YAMLError[],
+    fullContentRange: Range,
+): YamlParsingError[] {
+    return errors.map(({ message, linePos }) => {
+        const startPosition =
+            linePos == undefined
+                ? fullContentRange.start
+                : new Position(linePos[0].line - 1, linePos[0].col - 1);
+        const endPosition =
+            linePos == undefined
+                ? fullContentRange.end
+                : linePos[1]
+                  ? new Position(linePos[1].line - 1, linePos[1].col - 1)
+                  : startPosition;
+
+        return {
+            message,
+            range: new Range(startPosition, endPosition),
+            severity: "ERR",
+        };
+    });
 }
