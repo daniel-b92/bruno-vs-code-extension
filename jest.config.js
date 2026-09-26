507d44aa300520cb5b71a222fc96a0f6e65725ae
daniel-b92 <danielbeicht@web.de>
2026-09-26 17:25:05 +0200

    merge different jest configs into a single one (#277)
    * merge different jest configs into a single one
    
    * temporarily break test to check ci behavior
    
    * Revert "temporarily break test to check ci behavior"
    
    This reverts commit 86d83698e0a981dcc2f9da6393b4b78628acbf38.

---
+++ jest.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+
+module.exports = {
+    projects: [
+        {
+            displayName: "server",
+            testEnvironment: "node",
+            rootDir: path.resolve(__dirname, "server"),
+            transform: {
+                "^.+\\.[tj]s$": [
+                    "ts-jest",
+                    {
+                        tsconfig: path.resolve(
+                            __dirname,
+                            "server/tsconfig.json",
+                        ),
+                    },
+                ],
+            },
+            transformIgnorePatterns: ["<rootDir>/node_modules/(?!(watcher)/)"],
+            moduleNameMapper: {
+                "^@global_shared$": path.resolve(__dirname, "shared/index.ts"),
+                "^@global_shared/(.*)$": path.resolve(__dirname, "shared/$1"),
+            },
+            testMatch: [
+                "<rootDir>/src/**/*.test.ts",
+                "<rootDir>/src/**/*.spec.ts",
+            ],
+            moduleFileExtensions: ["ts", "tsx", "js", "json"],
+        },
+        {
+            displayName: "shared",
+            testEnvironment: "node",
+            rootDir: path.resolve(__dirname, "shared"),
+            transform: {
+                "^.+\\.[tj]s$": [
+                    "ts-jest",
+                    {
+                        tsconfig: path.resolve(
+                            __dirname,
+                            "shared/tsconfig.json",
+                        ),
+                    },
+                ],
+            },
+            transformIgnorePatterns: ["<rootDir>/node_modules/(?!(watcher)/)"],
+            testMatch: ["<rootDir>/**/*.test.ts", "<rootDir>/**/*.spec.ts"],
+            moduleFileExtensions: ["ts", "tsx", "js", "json"],
+        },
+    ],
+};
